47758bb8e3da9844feeae5ee4c396fa6e0f444f9
Miguel21S <leaismiguelss@gamail.com>
2024-04-28 23:37:22 +0200

    Creación: métodos de registro y login

--- Planificacion-Viajes/src/services/rootss.js
+++ Planificacion-Viajes/src/services/rootss.js
@@ -1,6 +1,7 @@
 
 const root = "http://localhost:2100/api/";
 
+////////////////  RUTA REGISTRARSE  /////////////////////////////
 export const RegitrarUser = async (user) => {
     const options = {
         method: "POST",
@@ -11,8 +12,37 @@ export const RegitrarUser = async (user) => {
     }
 
     try {
-        const response =await fetch(`${root}auth/register`, options);
+        const response = await fetch(`${root}auth/register`, options);
+        const data = response.json();
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
     } catch (error) {
-        
+        return error;
+    }
+}
+
+////////////////  RUTA LOGIN  /////////////////////////////
+export const Login = async (user) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    }
+
+    try {
+        const response = await fetch(`${root}auth/loguear`, options);
+        const data = response.json();
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
+    } catch (error) {
+        return error;
     }
 }
